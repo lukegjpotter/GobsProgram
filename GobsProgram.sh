0061edc3689de9e79bd4c9a6e4d5f824e312aef1
Luke GJ Potter <lukegjpotter@gmail.com>
2013-01-21 22:37:10 +0000

    First Commit

---
+++ GobsProgram.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+word="Penus"
+cols=6
+rows=8
+
+
+echo "Gob's Program: Y/N ? "
+read ans
+
+if [ "$ans" == "Y" ]
+then
+	for (( i=1; i<=$rows; i++ ))
+	do
+		str="$word"
+		
+		for (( j=1; j<=$cols; j++ ))
+		do
+			str="$str $word"
+		done
+		
+		echo -e $str
+	done
+fi
